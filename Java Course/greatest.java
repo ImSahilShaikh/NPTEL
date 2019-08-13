affddc1cb498985dda062825d344f2fe6a780f53
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-08-13 19:34:10 +0530

    Create greatest.java

---
+++ Java Course/greatest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;  
+public class greatest
+{
+        public static void main(String[] args) 
+        {
+          Scanner s = new Scanner(System.in); 
+          int x = s.nextInt(); 
+          int y = s.nextInt();
+          int z = s.nextInt();
+          int result = 0;
+          //Use if...else ladder to find the largest among 3 numbers and store the largest number in a variable called result.
+          if(x>y && x>z)
+            System.out.print(x);
+          else if(y>x && y>z)
+            System.out.print(y);
+          else
+            System.out.print(z);
+        }
+}
