67ee19a38e18cfd0a2ff2d0a9f6c03ada055af93
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-08-13 20:01:39 +0530

    Create DivBy3.java

---
+++ Java Course/DivBy3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class DivBy3
+{
+  public static void main(String[] args) 
+  {
+    Scanner sc = new Scanner(System.in);
+    int n=sc.nextInt();
+    int sum=0;
+
+    //Use for or while loop do the operation
+
+    int i=0,nos=0;
+
+    while(i<=n)
+    {
+        if(nos%2==0)
+        {
+                if(nos%3==0)
+                {
+                    sum=sum+nos;
+
+                }
+            i++;	
+        }
+        nos++;
+    }
+    System.out.print(sum)
+    }
+}
