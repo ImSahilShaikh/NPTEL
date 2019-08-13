c6b82f3bc47448cca3ab49232dab87e4273c4b6a
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-08-14 00:08:18 +0530

    Add files via upload

---
+++ Java Course/Week2/school.java
@@ -0,0 +1,31 @@
+ // This is the class named School
+class School { 
+    // This is a method in class School
+    public void print() { 
+		System.out.println("Hi! I class SCHOOL."); 
+    } 
+} 
+// This is the class named Student
+class Student { 
+	// This is a method in class Student
+    public void print() { 
+		System.out.println("Hi! I am class STUDENT"); 
+    } 
+}
+
+public class school{ 
+    public static void main(String[] args) { 
+
+
+// Create an object of class Student
+	Student s1 = new Student() ;
+
+// Call 'print()' method of class Student 
+	s1.print();
+// Create an object of class School
+	School s2 = new School();
+// Call 'print()' method of class School
+	s2.print();
+	     } 
+}
+
