2a751b5d26bf1794085fb1019012331b846c329c
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-08-13 20:29:18 +0530

    Create str.java

---
+++ Java Course/Week2/str.java
@@ -0,0 +1,14 @@
+public class str{ 
+    public static void main(String[] args) { 
+
+//Declare variable with name 'nptel', 'space' and 'java' and proper datatype.
+String nptel,space,java;
+
+//Initialize the variables with proper input
+nptel=new String("NPTEL");
+space=new String(" ");
+java=new String("JAVA");
+
+   System.out.print(nptel+space+java);
+   }
+}
