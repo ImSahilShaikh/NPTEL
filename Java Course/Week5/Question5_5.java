d666d559724085f65ca867580e74b6247656c90d
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-09-02 13:51:07 +0530

    Create Question5_5.java

---
+++ Java Course/Week5/Question5_5.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Question5_5
+{
+	public static void main (String   args[ ] )
+  {
+    Scanner scan = new Scanner(System.in);
+    int i = scan.nextInt();
+    int j; 
+    
+    // Put the following code under try-catch block to handle exceptions
+    try{
+    switch (i) 
+    {
+      case 0 : 
+        int zero = 0; 
+        j = 92/ zero; 		
+		  break;
+      case 1 : 
+        int b[ ] = null; 
+        j = b[0] ; 	
+      break;
+      default:
+	       System.out.print("No exception");
+	 }
+}
+catch(ArithmeticException ae)
+{
+  System.out.print("java.lang.ArithmeticException: / by zero");
+}
+catch(NullPointerException ne)
+{
+  System.out.print("java.lang.NullPointerException");
+}
