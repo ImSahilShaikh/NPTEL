8134fefaefb28ffe8f75a2ba6e2e35d030895437
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-09-02 13:40:58 +0530

    Create Question5_4.java

---
+++ Java Course/Week5/Question5_4.java
@@ -0,0 +1,29 @@
+//Prefixed Fixed Code:
+import java.util.*;
+public class Question5_4
+{
+  public static void main(String[] args) 
+  {
+    Scanner sc = new Scanner(System.in); 
+    int length = sc.nextInt(); 
+
+    // create an array to save user input 
+    int[] name = new int[length];
+    int sum=0;//save the total sum of the array.
+    
+    //Define try-catch block to save user input in the array "name",if there is an exception then catch the exception otherwise print the total sum of the array.    
+		try
+    {
+      for(int i=0;i<name.length;i++)
+      {
+        name[i]=sc.nextInt();
+        sum=sum+name[i];
+      }
+      System.out.print(sum); 
+    }
+		catch(InputMismatchException me)
+    {
+      System.out.print("You entered bad data.");
+    }
+  }
+}
