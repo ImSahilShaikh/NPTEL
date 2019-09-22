c773beae5fc332a8db0e90346dde4fcf6d64fb9e
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-09-23 01:37:48 +0530

    Create Question5.java

---
+++ Java Course/Week7/Question5.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Question5 
+{  
+	public static void main(String[] args) 
+	{ 
+		try
+		{
+			String s1="NPTELJAVA"; 
+			Scanner inr = new Scanner(System.in);
+			int n = inr.nextInt();
+            char c='a';
+			
+			//Replace the char in string "s1" with the char 'a' at index "n"  and print the modified string 
+		
+			StringBuilder mystring=new StringBuilder(s1);
+			mystring.setCharAt(n,c);
+			System.out.print(mystring);	
+		}
+		catch (Exception e)
+		{
+			System.out.println("exception occur");
+	    }	   
+	}  
+}  
