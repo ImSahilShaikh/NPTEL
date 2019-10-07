a434f62300f5a87fc9deef62c57c68326eefc100
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-10-07 20:49:12 +0530

    Create RenameTable.java

---
+++ Java Course/Week10/RenameTable.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+import java.lang.*;
+public class RenameTable {
+    public static void main(String args[]) {
+        try {
+              Connection conn = null;
+              Statement stmt = null;
+              String DB_URL = "jdbc:sqlite:/tempfs/db";
+              System.setProperty("org.sqlite.tmpdir", "/tempfs");
+            
+              // Open a connection
+              conn = DriverManager.getConnection(DB_URL);
+              stmt = conn.createStatement();
+	      
+	      // Write the SQL command to rename a table
+		String query="alter table players RENAME to SPORTS";
+		// Execute the SQL command
+	  	stmt.executeUpdate(query);
+	        }
+       catch(Exception e){ System.out.println(e);}  
+    }
+}
