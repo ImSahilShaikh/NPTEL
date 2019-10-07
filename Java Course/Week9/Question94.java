1f22f43160bd44b535b5a95cf29830dcaa7d87e8
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-10-07 20:18:15 +0530

    Create Question94.java

---
+++ Java Course/Week9/Question94.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Question94
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		// Declare necessary variables
+
+		// Input 5x5 2D Array using Scanner Class
+
+		// Perform the reflection operation
+
+		// Output 5x5 2D Reflection Array
+		String arr[]=new String[5];
+    		for(int i=0;i<5;i++)
+        		arr[i]=sc.nextLine();
+    
+		char matrix[][]=new char[5][5];
+		for(int i=0;i<5;i++)
+		{
+			char[] chararray=arr[i].toCharArray();
+  			for(int j=0;j<5;j++)
+			{
+  				matrix[i][j]=chararray[j];
+  			}
+	}
+	for(int i=0;i<5;i++)
+	{
+    		for(int j=4;j>=0;j--)
+        	System.out.print(matrix[i][j]);
+    		System.out.println();
+	}
+	} // The main() method ends here
+} // The main class ends here
