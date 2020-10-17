b8554daf882958e37fc51bbe4d7b760d9e24790a
Akash Rokade <akashdrokade@gmail.com>
2020-10-17 11:55:43 +0530

    updated code

--- Java Course/Week1/Armstrongs.java
+++ Java Course/Week1/Armstrongs.java
@@ -1,28 +1,17 @@
-import java.util.Scanner;
-public class Exercise1_4 
-{
-  public static void main(String[] args) 
-  {
-    Scanner sc = new Scanner(System.in);
-    int n=sc.nextInt();
-    int result=0;
-    int temp=n;	
-    int sum=0; 
-    //Use while loop check the number is Armstrong or not.
-    while(n>0)
-    {
-      result=n%10;
-        sum=sum+(result*result*result);
-        n=n/10;
-    }
+class ArmstrongExample{  
+  public static void main(String[] args)  {  
+    int c=0,a,temp;  
+    int n=153;//It is the number to check armstrong  
+    temp=n;  
+    while(n>0)  
+    {  
+    a=n%10;  
+    n=n/10;  
+    c=c+(a*a*a);  
+    }  
+    if(temp==c)  
+    System.out.println("armstrong number");   
+    else  
+        System.out.println("Not armstrong number");   
+   }  
 
-    //store the output(1 or 0) in result variable.
-
-    if(temp==sum)
-    {
-      System.out.print("1");
-    }
-    else
-      System.out.print("0");
-   }
-}
